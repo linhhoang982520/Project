8b27f8e71321d80117c2abcb94d3b18277606b4c
linhhoang982520 <49657938+linhhoang982520@users.noreply.github.com>
2019-06-12 10:26:25 -0300

    Project
    Co-Authored-By: Chris Cusack - NBCC <chrisecusack@users.noreply.github.com>

---
+++ NBCC.CardGame/NBCC.CardGame/Deck.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC.CardGame
+{
+    public class Deck
+    {
+        private List<Card> deck = new List<Card>();
+
+        public Hand DealHand(int number)
+        {
+            if (deck.Count == 0)
+                throw new ConstraintException("There are no cards left in the deck");
+            Hand hand = new Hand();
+
+            int countTo = deck.Count >= number ? number : deck.Count;
+
+            for(int handIndex = 0; handIndex < countTo; handIndex++)
+            {
+                hand.AddCard(deck[0]);
+                deck.RemoveAt(0);
+            }
+
+            return hand;
+        }
+
+        public Deck()
+        {
+            MakeDeck();
+        }
+
+        public Card DrawOneCard()
+        {
+            Card topCard;
+
+            if (deck.Count > 0)
+            {
+                //assign top card
+                topCard = deck[0];
+
+                //adjust your deck
+                deck.RemoveAt(0);
+            }
+            else
+                throw new ArgumentException("No more card in deck");
+
+            return topCard;
+        }
+
+        private void MakeDeck()
+        {
+            //Load the deck with all cards 
+            //There are 4 suits
+            for(int pickASuit = 0; pickASuit <= 3; pickASuit++)
+            {
+                //13 persuits
+                for(int pickAValue = 0; pickAValue <= 12; pickAValue++)
+                {
+                    Card newCard = new Card((Suit)pickASuit, (FaceValue)pickAValue);
+
+                    deck.Add(newCard);
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rGen = new Random();
+            List<Card> newDeck = new List<Card>();
+
+            // while loop through our deck cards
+            while(deck.Count > 0)
+            {
+                int removerIndex = rGen.Next(0, deck.Count);
+                Card removeCard = deck[removerIndex];
+                deck.RemoveAt(removerIndex);
+
+                //Add newDeck
+                newDeck.Add(removeCard);
+            }
+
+            deck = newDeck;
+        }
+    }
+}
---
+++ NBCC.CardGame/NBCC.CardGame/Hand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC.CardGame
+{
+    public class Hand
+    {
+        private List<Card> cards = new List<Card>();
+
+        public int Count { get { return cards.Count(); } }
+
+        public Card this[int index]
+        {
+            get
+            {
+                return cards[index];
+            }
+        }
+
+        public void AddCard (Card newCard)
+        {
+            if (ConstainCard(newCard))
+            {
+                throw new ConstraintException(newCard.FaceValue.ToString() + " of " + newCard.ToString() + " already exist in the Hands");
+            }
+
+            cards.Add(newCard);
+        }
+
+        private bool ConstainCard(Card cardToCheck)
+        {
+            bool result = cards.Where(c => c.FaceValue == cardToCheck.FaceValue && c.Suit == cardToCheck.Suit).Count() > 0;
+            return result;
+        }
+
+        public void RemoveCard(Card theCard)
+        {
+            if (cards.Contains(theCard))
+            {
+                cards.Remove(theCard);
+            }
+            else
+            {
+                throw new ConstraintException("The card does not exist in the Hands");
+            }
+        }
+        
+        public void RemoveCard(int index)
+        {
+            if(index <= cards.Count - 1)
+            {
+                cards.RemoveAt(index);
+            }
+            else
+            {
+                throw new DataException("Index value exceeds the number of cards in the hand");
+            }
+        }
+
+        public void RemoverCard(Suit theSuit, FaceValue theFaceValue)
+        {
+            foreach(Card card in cards)
+            {
+                if(card.Suit == theSuit && card.FaceValue == theFaceValue)
+                {
+                    cards.Remove(card);
+                    return;
+                }
+            }
+            throw new DataException(theFaceValue.ToString() + " of " + theSuit.ToString());
+        }
+    }
+}
